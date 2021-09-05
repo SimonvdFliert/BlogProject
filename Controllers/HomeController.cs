b8c234988c91395fd511b7e60edd58010003764c
SimonvdFliert <Simonvdfliert@hotmail.com>
2021-09-05 16:20:18 +0200

    adding layout

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,10 +23,17 @@ namespace BlogProject.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
